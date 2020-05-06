7e117f12150189ca52dc341c73c66214b2eb011b
Dima Alhovik <halapsikon@mail.ru>
2020-05-06 10:51:23 +0300

    make files more beautiful

--- 5-6 cuda picture/picture/picture/kernel.cu
+++ 5-6 cuda picture/picture/picture/kernel.cu
@@ -82,17 +82,17 @@ void cpu_work(const unsigned char* imgIn, unsigned char* imgOut, int width, int 
 	{
 		for (int j = 1; j < height - 1; ++j)
 		{
-			temp[0] = imgIn[(j - 1)*width + (i - 1)];
+			//temp[0] = imgIn[(j - 1)*width + (i - 1)];
 			temp[1] = imgIn[(j - 1)*width + (i + 0)];
-			temp[2] = imgIn[(j - 1)*width + (i + 1)];
+			//temp[2] = imgIn[(j - 1)*width + (i + 1)];
 			
 			temp[3] = imgIn[(j + 0)*width + (i - 1)];
-			temp[4] = imgIn[(j + 0)*width + (i + 0)];
+			//temp[4] = imgIn[(j + 0)*width + (i + 0)];
 			temp[5] = imgIn[(j + 0)*width + (i + 1)];
 			
-			temp[6] = imgIn[(j + 1)*width + (i - 1)];
+			//temp[6] = imgIn[(j + 1)*width + (i - 1)];
 			temp[7] = imgIn[(j + 1)*width + (i + 0)];
-			temp[8] = imgIn[(j + 1)*width + (i + 1)];
+			//temp[8] = imgIn[(j + 1)*width + (i + 1)];
 
 			imgOut[j*width + i] = stampingEffect(temp);
 		}
@@ -100,36 +100,13 @@ void cpu_work(const unsigned char* imgIn, unsigned char* imgOut, int width, int 
 
 	QueryPerformanceCounter(&finish);
 	delay = (finish.QuadPart - start.QuadPart) * 1000.0f / frequency.QuadPart;
-	printf("The time for cpu spend: %.3f ms\n", delay);
+	printf("The time for cpu spend: %.3f ms\n\n", delay);
 }
 
 //<----------------------------------GPU------------------------------------->
 
 __global__ void stampingEffectCuda(unsigned char* src, unsigned char* dst, int width, int height)
 {
-	//const int offsetX = BLOCK_X*blockIdx.x*THREAD_ELEMENT_X + threadIdx.x*THREAD_ELEMENT_X;
-	//const int offsetY = BLOCK_Y*blockIdx.y*THREAD_ELEMENT_Y + threadIdx.y*THREAD_ELEMENT_Y;
-
-	//int a = src[(offsetY + 0)*rowLength + offsetX];
-	//int b = src[(offsetY + 1)*rowLength + offsetX];
-	//int c = src[(offsetY + 2)*rowLength + offsetX];
-	//int d = src[(offsetY + 3)*rowLength + offsetX];
-
-	///*int startWriteIndex = offsetY*rowLength + offsetX*THREAD_ELEMENT_Y;
-
-	//dst[startWriteIndex+0] = a;
-	//dst[startWriteIndex+1] = b;
-	//dst[startWriteIndex+2] = c;
-	//dst[startWriteIndex+3] = d;*/
-
-
-	//const int offsetX_out = BLOCK_Y*blockIdx.x*THREAD_ELEMENT_Y + threadIdx.x*THREAD_ELEMENT_Y;
-	//const int offsetY_out = BLOCK_X*blockIdx.y*THREAD_ELEMENT_X + threadIdx.y*THREAD_ELEMENT_X;
-
-	//dst[offsetY_out*rowLength * 4 + offsetX_out + 0] = a;
-	//dst[offsetY_out*rowLength * 4 + offsetX_out + 1] = b;
-	//dst[offsetY_out*rowLength * 4 + offsetX_out + 2] = c;
-	//dst[offsetY_out*rowLength * 4 + offsetX_out + 3] = d;
 
 	int offsetX = blockIdx.x*blockDim.x + threadIdx.x;
 	int offsetY = blockIdx.y*blockDim.y + threadIdx.y;
@@ -192,25 +169,34 @@ void cuda_work(unsigned char* imgIn, unsigned char* imgOut, int width, int heigh
 	float elapsedTime;
 	cudaCheckStatus(cudaEventElapsedTime(&elapsedTime, start, stop));
 
-	printf("The time for cuda with global memory spend: %.3f ms\n", elapsedTime);
+	printf("The time for cuda with global memory spend: %.3f ms\n\n", elapsedTime);
 
 	cudaCheckStatus(cudaEventDestroy(start));
 	cudaCheckStatus(cudaEventDestroy(stop));
 
-	printf("problem1");
 	unsigned char* temp = new unsigned char[widthGpu*heightGpu];
 	cudaCheckStatus(cudaMemcpy2D(imgOut, width, deviceOutMatrix, widthGpu, width, height, cudaMemcpyDeviceToHost));
-	printf("problem2\n");
 
 	cudaCheckStatus(cudaFree(deviceInMatrix));
 	cudaCheckStatus(cudaFree(deviceOutMatrix));
 }
 
+//checking
+
+bool compareTwoPictures(unsigned char* first, unsigned char* second, int width, int height)
+{
+	for (int i = 0; i < width*height; ++i)
+	{
+		if (first[i] != second[i])
+		{
+			return false;
+		}
+	}
+	return true;
+}
 
 int main()
 {
-	printf("start\n");
-
 	char* imagePathInput = "mountain.pgm";
 	char* imagePathResCpu = "mountain_outCPU.pgm";
 	char* imagePathResGpu = "mountain_outGPU.pgm";
@@ -224,7 +210,7 @@ int main()
 	unsigned char* imgGPUOut = (unsigned char*)malloc(widthImage * heightImage * sizeof(unsigned char));
 	unsigned char* imgGPUWithSharedOut = (unsigned char*)malloc(widthImage * heightImage * sizeof(unsigned char));
 
-	printf("Input image is \"%s\" - size: %dX%d - channels: %d\n", imagePathInput, widthImage, heightImage, channels);
+	printf("Input image is \"%s\" - size: %dX%d - channels: %d\n\n", imagePathInput, widthImage, heightImage, channels);
 
 	//ADD FRAME
 	unsigned int width = widthImage + 2;
@@ -242,8 +228,17 @@ int main()
 	deleteFrameFromoImage(temp_GPU, imgGPUOut, width, height);
 
 	__savePPM(imagePathResCpu, imgCPUOut, widthImage, heightImage, channels);
+	printf("SAVED CPU Picture\n");
 	__savePPM(imagePathResGpu, imgGPUOut, widthImage, heightImage, channels);
+	printf("SAVED GPU Picture\n");
 
-	printf("end\n");
+	printf("\nRESULT OF COMPARE CUDA AND CPU: %s\n", (compareTwoPictures(imgCPUOut, imgGPUOut, widthImage, heightImage) ? "+" : "-"));
+
+	free(inputImg);
+	free(imgCPUOut);
+	free(imgGPUOut);
+	free(imgGPUWithSharedOut);
+	free(temp_CPU);
+	free(temp_GPU);
 }
 
